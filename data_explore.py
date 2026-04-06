dcd73f8da73af452e79d45f5d65df9b74aa9badd
Mc-ler <change-dc@qq.com>
2026-04-06 23:52:57 +0800

    just explore data frame at first

--- data_explore.py
+++ data_explore.py
@@ -1,1 +1,17 @@
+# import needy models
 import csv
+import datetime
+from matplotlib import pyplot as plt
+
+#read data from file and explore data frame
+filename = "./源代码文件/chapter_16/Temp_Pri/data_ori.csv"
+with open(filename) as f:
+    read_data = csv.reader(f)
+    header = next(read_data)        # get the header row
+    t = 0
+    for line in read_data:
+        print("station:{},date:{},H:{},L:{}".format(line[0],line[1],line[2],line[3]))
+        t += 1
+        if t == 5:
+            break
+        
