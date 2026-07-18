40353d18ed5b62fa1b59a22ea5789ae341b9d3ad
Mc-ler <change-dc@qq.com>
2026-07-18 11:16:59 +0800

    A slid change: Move out background images

--- graph_describe.py
+++ graph_describe.py
@@ -25,7 +25,7 @@ def data_read(data:list,station_ID):
                 s_emxt.append(L)
     return s_date, s_emnt, s_emxt
 
-filename = "./源代码文件/chapter_16/Temp_Pri/data_ori.csv"
+filename = "./chapter_16/Temp_Pri/data_ori.csv"
 with open(filename) as f:
     read_data = list(csv.reader(f))
     # header = next(read_data)        # get the header row
@@ -34,8 +34,8 @@ with open(filename) as f:
     s1_date, s1_emnt, s1_emxt = data_read(read_data,"CHM00057127")
     s2_date, s2_emnt, s2_emxt = data_read(read_data,"CHM00057131")
 
-img_1 = Image.open("./源代码文件/chapter_16/Temp_Pri/image/figure_1_1.jpg")
-img_2 = Image.open("./源代码文件/chapter_16/Temp_Pri/image/figure_1_2.jpg")
+# img_1 = Image.open("./源代码文件/chapter_16/Temp_Pri/image/figure_1_1.jpg")
+# img_2 = Image.open("./源代码文件/chapter_16/Temp_Pri/image/figure_1_2.jpg")
 def get_extent(dt_list, y_low, y_high):
     left = dt_list[0]
     right = dt_list[-1]
@@ -59,7 +59,7 @@ ax[0].set_xticks(s1_date[::5])
 ax[0].set_xticklabels(s1_date[::5], rotation=60, ha='right')
 ax[0].set_xlabel("Date(Year)", fontsize=14, labelpad=10)
 ax[0].set_ylabel("Temperature($^\circ$C)",fontsize=14, labelpad=10)
-ax[0].imshow(img_2, extent=get_extent(s1_date, min(s1_emnt), max(s1_emxt)), aspect='auto', zorder=0)
+# ax[0].imshow(img_2, extent=get_extent(s1_date, min(s1_emnt), max(s1_emxt)), aspect='auto', zorder=0)
 
 ax[1].plot(s2_date, s2_emnt, c='#0073ad', alpha=0.8)
 ax[1].plot(s2_date, s2_emxt, c='#123057', alpha=0.8)
@@ -68,13 +68,13 @@ ax[1].set_xticks(s1_date[::5])
 ax[1].set_xticklabels(s1_date[::5], rotation=60, ha='right')
 ax[1].set_xlabel("Date(Year)", fontsize=14, labelpad=10)
 ax[1].set_ylabel("Temperature($^\circ$C)",fontsize=14, labelpad=10)
-ax[1].imshow(img_1, extent=get_extent(s2_date, min(s2_emnt), max(s2_emxt)), aspect='auto', zorder=0)
+# ax[1].imshow(img_1, extent=get_extent(s2_date, min(s2_emnt), max(s2_emxt)), aspect='auto', zorder=0)
 
 
 title = f"Monthly High Tempeture VS. Low Temp Of Two Stations".title()
 fig.text(0.5, 0.9, title, fontsize=18, ha='center', transform=fig.transFigure)
 fig.autofmt_xdate()
-fig.savefig("./源代码文件/chapter_16/Temp_Pri/image/figure_1_3.svg", dpi=300, format='svg')
+# fig.savefig("./源代码文件/chapter_16/Temp_Pri/image/figure_1_3.svg", dpi=300, format='svg')
 plt.show()
 
  
